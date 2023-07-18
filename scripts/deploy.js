8ce97972a94cebab678d7fb5e6d808d206f50659
sfyr111 <sfyr111@163.com>
2023-07-18 22:47:05 +0800

    deploy smart contract to localhost network

---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners(); // getSigners() returns an array of signers
+  const accountBalance = await deployer.getBalance();
+
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
+
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0); // exit Node process without error
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+  }
+  // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+}
+
+runMain();
