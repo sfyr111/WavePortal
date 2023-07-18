5f5647213189a3fd1a85e4862db5d994a64575f9
sfyr111 <sfyr111@163.com>
2023-07-18 22:31:13 +0800

    store data on smart contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,22 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners(); // getSigners() returns an array of signers
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  const firstWaveTxn = await waveContract.wave();
+  await firstWaveTxn.wait(); // wait for the transaction to be mined
+
+  await waveContract.getTotalWaves();
+
+  const secondWaveTxn = await waveContract.connect(randomPerson).wave();
+  await secondWaveTxn.wait();
+
+  await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
